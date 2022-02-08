58da9abc1b96f66396ad55943d45310358ef5714
Eugene Zemskov <eugenez@sentientvision.com>
2022-02-08 17:59:34 +1100

    Viewer : Format some VMTI metadata

--- viewer/src/main/java/org/jmisb/viewer/MetadataPanel.java
+++ viewer/src/main/java/org/jmisb/viewer/MetadataPanel.java
@@ -3,6 +3,14 @@ package org.jmisb.viewer;
 import org.jmisb.api.klv.st0601.IUasDatalinkValue;
 import org.jmisb.api.klv.st0601.UasDatalinkMessage;
 import org.jmisb.api.klv.st0601.UasDatalinkTag;
+import org.jmisb.api.klv.st0601.NestedVmtiLocalSet;
+import org.jmisb.api.klv.st0903.VmtiLocalSet;
+import org.jmisb.api.klv.st0903.VmtiMetadataKey;
+import org.jmisb.api.klv.st0903.IVmtiMetadataValue;
+import org.jmisb.api.klv.st0903.VTargetSeries;
+import org.jmisb.api.klv.st0903.vtarget.VTargetPack;
+import org.jmisb.api.klv.st0903.vtarget.VTargetMetadataKey;
+
 import org.jmisb.api.video.IMetadataListener;
 import org.jmisb.api.video.MetadataFrame;
 import org.slf4j.Logger;
@@ -40,6 +48,58 @@ public class MetadataPanel extends JTextPane implements IMetadataListener
         putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true);
     }
 
+    private String tryFormatVmtiLS(IUasDatalinkValue value)
+    {
+        if (value instanceof NestedVmtiLocalSet == false)
+        {
+            return "";
+        }
+
+        VmtiLocalSet vmtiLS = ((NestedVmtiLocalSet)value).getVmti();
+        return formatVmtiLS(vmtiLS);
+    }
+
+    private String formatVmtiLS(VmtiLocalSet vmti)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (VmtiMetadataKey tag : vmti.getTags())
+        {
+            IVmtiMetadataValue value = vmti.getField(tag);
+            String targetsStr = tryFormatTargetSeries(value);
+            if (targetsStr != "")
+            {
+                sb.append(targetsStr);
+            }
+            else
+            {
+                sb.append("<b>VMTI-").append(tag).append(":</b> ").append(value.getDisplayableValue()).append("<br>");
+            }
+        }
+        return sb.toString();
+    }
+
+    private String tryFormatTargetSeries(IVmtiMetadataValue value)
+    {
+        if (value instanceof VTargetSeries == false)
+        {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        java.util.List<VTargetPack> tgtList = ((VTargetSeries)value).getVTargets();
+        for (VTargetPack target : tgtList)
+        {
+            sb.append("id ").append(target.getTargetIdentifier()).append("<br>");
+            for (VTargetMetadataKey tag : target.getTags())
+            {
+                IVmtiMetadataValue tgtValue = target.getField(tag);
+                sb.append(tag).append(": ").append(tgtValue.getDisplayableValue()).append("<br>");
+            }
+        }
+
+        return sb.toString();
+    }
+
     @Override
     public void onMetadataReceived(MetadataFrame metadataFrame)
     {
@@ -70,7 +130,17 @@ public class MetadataPanel extends JTextPane implements IMetadataListener
                     for (UasDatalinkTag tag : uasDatalinkMessage.getTags())
                     {
                         IUasDatalinkValue value = uasDatalinkMessage.getField(tag);
-                        sb.append("<b>").append(tag).append(":</b> ").append(value.getDisplayableValue()).append("<br>");
+                        
+                        String vmtiStr = tryFormatVmtiLS(value);
+                        if (vmtiStr != "")
+                        {
+                            sb.append(vmtiStr);
+                        }
+                        else
+                        {
+                            sb.append("<b>").append(tag).append(":</b> ").append(value.getDisplayableValue()).append("<br>");
+                        }
+                                    
                     }
                 }
 
